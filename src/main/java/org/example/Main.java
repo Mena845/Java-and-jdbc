90ab818f944c9b3f49ed51ac53704ccd4f006eb3
mena <tsikiniainatsilavina@gmail.com>
2025-12-01 17:15:00 +0300

    Initial commit - projet Java JDBC

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:postgresql://localhost:5432/expense";
+        String user = "postgres";
+        String password = "tsilakely2220";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connexion réussie !");
+        } catch (SQLException e) {
+            System.out.println("Erreur : " + e.getMessage());
+        }
+    }
+}
